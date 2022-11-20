de7a394c531130c2e0b4b72964bf84c1e347961b
Aryaman <someoneg4@gmail.com>
2022-11-20 15:52:01 +0530

    a whole ass 1.2

---
+++ processScheduling.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sched.h>
+
+void processA() {
+    struct timespec start;
+    struct timespec stop;
+    double billion = 1000000000;
+  
+    clock_gettime(CLOCK_REALTIME, &start);
+    pid_t pid = fork();
+    int stat;
+    if (pid==0) {
+        struct sched_param paramA;
+        paramA.sched_priority = 0;
+        int setRes = sched_setscheduler(0, SCHED_OTHER, &paramA);
+        if (setRes!=0) {perror("A: Error");}
+        
+        char *arr1[48] = {"cd", "~/kernelbuild/linux-5.19.81/linux-5.19.8"};
+        char *arr2[16] = {"make", "mrproper"};
+        char *arr3[8] = {"cd", ".."};
+        char *arr4[48] = {"cp", ".config", "linux-5.19.81/linux-5.19.8"};
+        char *arr5[48] = {"cd", "linux-5.19.81/linux-5.19.8"};
+        char *arr6[8] = {"make"};
+        execvp("/bin/cd", arr1);
+        execvp("/bin/make", arr2);
+        execvp("/bin/cd", arr3);
+        execvp("/bin/cp", arr4);
+        execvp("/bin/cd", arr5);
+        execvp("/bin/make", arr6);
+
+    } else if(pid>0) {
+        waitpid(pid, &stat, 0);
+        clock_gettime(CLOCK_REALTIME, &stop);
+
+        double duration = stop.tv_sec + stop.tv_nsec/billion - (start.tv_sec + start.tv_nsec/billion);
+
+        printf("Process A runtime: %lf\n", duration);
+
+    } else {
+        puts("Process A failed to fork!");
+    }
+}
+
+int main() {
+    processA();
+    return 0;
+}
