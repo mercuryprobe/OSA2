60fa48ef7977e6ab56b808aedf2080f3b6f6ea3e
Aryaman <someoneg4@gmail.com>
2022-11-20 17:37:55 +0530

    multi

--- processScheduling.c
+++ processScheduling.c
@@ -37,7 +37,71 @@ void processA() {
     }
 }
 
+void processB() {
+    struct timespec start;
+    struct timespec stop;
+    double billion = 1000000000;
+  
+    clock_gettime(CLOCK_REALTIME, &start);
+    pid_t pid = fork();
+    int stat;
+    if (pid==0) {
+        struct sched_param paramB;
+        paramB.sched_priority = 0;
+        int setRes =  1;
+        setRes = sched_setscheduler(0, SCHED_OTHER, &paramB);
+        if (setRes!=0) {perror("B: Error");}
+        
+        char *arr[16] = {"./runnerB.sh", NULL};
+        execvp("./runnerB.sh", arr);
+
+    } else if(pid>0) {
+        waitpid(pid, &stat, 0);
+        clock_gettime(CLOCK_REALTIME, &stop);
+
+        double duration = stop.tv_sec + stop.tv_nsec/billion - (start.tv_sec + start.tv_nsec/billion);
+
+        printf("Process B runtime: %lf\n", duration);
+
+    } else {
+        puts("Process B failed to fork!");
+    }
+}
+
+void processC() {
+    struct timespec start;
+    struct timespec stop;
+    double billion = 1000000000;
+  
+    clock_gettime(CLOCK_REALTIME, &start);
+    pid_t pid = fork();
+    int stat;
+    if (pid==0) {
+        struct sched_param paramC;
+        paramC.sched_priority = 0;
+        int setRes =  1;
+        setRes = sched_setscheduler(0, SCHED_OTHER, &paramC);
+        if (setRes!=0) {perror("C: Error");}
+        
+        char *arr[16] = {"./runnerC.sh", NULL};
+        execvp("./runnerC.sh", arr);
+
+    } else if(pid>0) {
+        waitpid(pid, &stat, 0);
+        clock_gettime(CLOCK_REALTIME, &stop);
+
+        double duration = stop.tv_sec + stop.tv_nsec/billion - (start.tv_sec + start.tv_nsec/billion);
+
+        printf("Process C runtime: %lf\n", duration);
+
+    } else {
+        puts("Process C failed to fork!");
+    }
+}
+
 int main() {
     processA();
+    processB();
+    processC();
     return 0;
 }
