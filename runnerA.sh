a43e11320da3012c0e6e90f42e59ba257a8c635b
Aryaman <someoneg4@gmail.com>
2022-11-20 16:54:17 +0530

    ty

--- runnerA.sh
+++ runnerA.sh
@@ -1,7 +1,4 @@
-cd kernel1build/linux-5.19.81/linux-5.19.8
-pwd
-make mrproper
-cd ..
+#!/bin/bash
+cd kernel1build/linux-5.19.81/linux-5.19.8 && make mrproper
 cp .config linux-5.19.81/linux-5.19.8
-cd linux-5.19.81/linux-5.19.8
-make -j2
+cd linux-5.19.81/linux-5.19.8 && make -j2
