f5c526ab386ee6105b71fab233adabd1e748ff98
Aryaman <someoneg4@gmail.com>
2022-11-20 16:04:37 +0530

    hm

--- processScheduling.c
+++ processScheduling.c
@@ -21,12 +21,14 @@ void processA() {
         if (setRes!=0) {perror("A: Error");}
 
         char *arr1[48] = {"cd", "~/kernelbuild/linux-5.19.81/linux-5.19.8"};
+        char *arr7[8] = {"pwd"};
         char *arr2[16] = {"make", "mrproper"};
         char *arr3[8] = {"cd", ".."};
         char *arr4[48] = {"cp", ".config", "linux-5.19.81/linux-5.19.8"};
         char *arr5[48] = {"cd", "linux-5.19.81/linux-5.19.8"};
         char *arr6[8] = {"make"};
         execvp("/bin/cd", arr1);
+        execvp("/bin/pwd", arr7);
         execvp("/bin/make", arr2);
         execvp("/bin/cd", arr3);
         execvp("/bin/cp", arr4);
