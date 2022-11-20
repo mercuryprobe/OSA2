70581faa547d12c566f46c34b92dcf5edb24bb0e
Aryaman <someoneg4@gmail.com>
2022-11-20 16:59:36 +0530

    dbg

--- runnerA.sh
+++ runnerA.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd kernel1build/linux-5.19.81/linux-5.19.8 && make mrproper
+cd kernelbuild/linux-5.19.81/linux-5.19.8 && make mrproper
 cp .config linux-5.19.81/linux-5.19.8
 cd linux-5.19.81/linux-5.19.8 && make -j2
