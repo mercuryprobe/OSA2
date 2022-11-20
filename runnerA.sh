2c055feca633da6556ec0b1627c429c4cd7b7658
Aryaman <someoneg4@gmail.com>
2022-11-20 17:13:18 +0530

    run

--- runnerA.sh
+++ runnerA.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/mercury/osAssignment/osAssignment2/OSA2/kernelbuild/linux-5.19.81/linux-5.19.8 && make mrproper
 cp /home/mercury/osAssignment/osAssignment2/OSA2/kernelbuild/.config /home/mercury/osAssignment/osAssignment2/OSA2/kernelbuild/linux-5.19.81/linux-5.19.8
-cd /home/mercury/osAssignment/osAssignment2/OSA2/kernelbuild/linux-5.19.81/linux-5.19.8 && make -j2
+cd /home/mercury/osAssignment/osAssignment2/OSA2/kernelbuild/linux-5.19.81/linux-5.19.8 && make -j2 
