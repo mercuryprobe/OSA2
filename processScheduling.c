5abf13b6033d908d938a29d324df29c741672ead
Aryaman <someoneg4@gmail.com>
2022-11-20 16:01:18 +0530

    bhai imma cry

--- processScheduling.c
+++ processScheduling.c
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sched.h>
+#include <time.h>
 
 void processA() {
     struct timespec start;
@@ -18,7 +19,7 @@ void processA() {
         paramA.sched_priority = 0;
         int setRes = sched_setscheduler(0, SCHED_OTHER, &paramA);
         if (setRes!=0) {perror("A: Error");}
-        
+
         char *arr1[48] = {"cd", "~/kernelbuild/linux-5.19.81/linux-5.19.8"};
         char *arr2[16] = {"make", "mrproper"};
         char *arr3[8] = {"cd", ".."};
