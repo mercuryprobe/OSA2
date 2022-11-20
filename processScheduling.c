6e8dac53f54915a6a44118e33bd77d64549eeef2
Aryaman <someoneg4@gmail.com>
2022-11-20 20:18:51 +0530

    big changes, 1.2 predone

--- processScheduling.c
+++ processScheduling.c
@@ -6,6 +6,97 @@
 #include <sched.h>
 #include <time.h>
 
+void compile3() {
+    struct timespec startA;
+    struct timespec stopA;
+    struct timespec startB;
+    struct timespec stopB;
+    struct timespec startC;
+    struct timespec stopC;
+
+    double billion = 1000000000;
+    int setRes =  1;
+
+    
+    pid_t pidA = fork();
+    int stat;
+
+    if (pidA==0) {
+        clock_gettime(CLOCK_REALTIME, &startA);
+        puts("Process A started!");
+        
+        struct sched_param paramA;
+        paramA.sched_priority = 0;
+        setRes = sched_setscheduler(0, SCHED_OTHER, &paramA);
+        if (setRes!=0) {perror("A: Error");}
+        
+        char *arr[16] = {"./runnerA.sh", NULL};
+        execvp("./runnerA.sh", arr);
+
+    } else if (pidA>0) {
+        pid_t pidB = fork();
+        int stat;
+
+        if (pidB==0) {
+            clock_gettime(CLOCK_REALTIME, &startB);
+            puts("Process B started!");
+
+            struct sched_param paramB;
+            paramB.sched_priority = 1;
+            setRes = sched_setscheduler(0, SCHED_RR, &paramB);
+            if (setRes!=0) {perror("B: Error");}
+            
+            char *arr[16] = {"./runnerB.sh", NULL};
+            execvp("./runnerB.sh", arr);
+
+        } else if (pidB>0) {
+            pid_t pidC = fork();
+            int stat;
+            
+            if (pidC==0) {
+                clock_gettime(CLOCK_REALTIME, &startC);
+                puts("Process C started!");
+
+                struct sched_param paramC;
+                paramC.sched_priority = 1;                
+                setRes = sched_setscheduler(0, SCHED_FIFO, &paramC);
+                if (setRes!=0) {perror("B: Error");}
+                
+                char *arr[16] = {"./runnerC.sh", NULL};
+                execvp("./runnerC.sh", arr);
+
+            } else if (pidC>0) {
+                int finishedThreads = 0;
+                while (finishedThreads<3) {
+                    int curID = waitpid(-1, &stat, 0);
+                    
+                    if (pidA==curID) {clock_gettime(CLOCK_REALTIME, &stopA);}
+                    if (pidA==curID) {clock_gettime(CLOCK_REALTIME, &stopB);}
+                    if (pidA==curID) {clock_gettime(CLOCK_REALTIME, &stopC);}
+                    finishedThreads -=1;
+
+                }
+            } else {
+                puts("Process C failed to fork!");
+            }
+        } else {
+            puts("Process B failed to fork!");
+
+        }
+
+        double durationA = stopA.tv_sec + stopA.tv_nsec/billion - (startA.tv_sec + startA.tv_nsec/billion);
+        double durationB = stopB.tv_sec + stopB.tv_nsec/billion - (startB.tv_sec + startB.tv_nsec/billion);
+        double durationC = stopC.tv_sec + stopC.tv_nsec/billion - (startC.tv_sec + startC.tv_nsec/billion);
+
+        printf("Process A runtime: %lf\n", durationA);
+        printf("Process B runtime: %lf\n", durationB);
+        printf("Process C runtime: %lf\n", durationC);
+
+    } else {
+        puts("Process A failed to fork!");
+    }
+}
+
 void processA() {
     struct timespec start;
     struct timespec stop;
@@ -15,6 +106,7 @@ void processA() {
     pid_t pid = fork();
     int stat;
     if (pid==0) {
+        puts("Process A started!"); 
         struct sched_param paramA;
         paramA.sched_priority = 0;
         int setRes =  1;
@@ -25,7 +117,7 @@ void processA() {
         execvp("./runnerA.sh", arr);
 
     } else if(pid>0) {
-        puts("Process A started!");
+        
         waitpid(pid, &stat, 0);
         clock_gettime(CLOCK_REALTIME, &stop);
 
