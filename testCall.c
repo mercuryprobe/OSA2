cf6829c07641be277e07ee1f895e1ec706f44cf1
Aryaman <someoneg4@gmail.com>
2022-11-24 02:36:05 +0530

    uh?

--- testCall.c
+++ testCall.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <linux/syscalls.h>
-#include <linux/kernel.h>
+// #include <linux/syscalls.h>
+// #include <linux/kernel.h>
 #define kernel_2d_memcpy 451
 
 int checker(int** given, int** actual, int nArr, int arrDepth) {
