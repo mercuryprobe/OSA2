8c8847ff567d7b50aa98febe7fc8387fc7e85988
Aryaman <someoneg4@gmail.com>
2022-11-24 02:40:35 +0530

    pray1

--- testCall.c
+++ testCall.c
@@ -5,41 +5,47 @@
 #define kernel_2d_memcpy 451
 
 int checker(int** given, int** actual, int nArr, int arrDepth) {
-    for (int i = 0; i<nArr; i++) {
-        for (int j = 0; j<arrDepth; j++) {
-            if (given[i][j] != actual[i][j]) {
-                return -1;
-            }
-        }
-    }
-
-    return 0;
+    
 }
 
 int main() {
-    int array[8][8];
-    int target[8][8];
+    int nArr = 8;
+    int arrDepth = 8;
+    int array[nArr][arrDepth];
+    int target[nArr][arrDepth];
     int k = 0;
-    for (int i = 0; i<8; i++) {
-        for (int j = 0; j<8; j++) {
+    for (int i = 0; i<nArr; i++) {
+        for (int j = 0; j<arrDepth; j++) {
             array[i][j] = k++;
         }
     }
 
-
     int callResult;
+    int fail = 0;
     callResult = syscall(451, array, target, 8, 8);
     if (callResult==0) {
-        if (checker(target, array, 8, 8)==0) {
+        for (int i = 0; i<nArr; i++) {
+            for (int j = 0; j<arrDepth; j++) {
+                if (given[i][j] != actual[i][j]) {
+                    fail = 1;
+                    break;
+                }
+            }
+        }
+
+        if (fail==0) {
             puts("Copy successful!");
         } else {
             puts("Copy failed!");
         }
+        
     } else if (callResult==-1) {
         puts("Syscall failed: __copy_to_user error!");
     } else {
         puts("Syscall failed: __copy_from_user error!");
     }
+
+    
     
     return 0;
 }
